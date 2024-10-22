5b2ed8e54087f058f98c57153b431a67c4c44494
Carl Wico <139624156+WCARL12@users.noreply.github.com.>
2024-10-22 19:30:17 -0230

    Finished Exercise 6-2

--- Labs/Lab-5/Exercise-6-2/mover.js
+++ Labs/Lab-5/Exercise-6-2/mover.js
@@ -1,29 +1,62 @@
 // "use strict";
 
-// const topButton = getElement("#top_btn");
-
 // getElement()
 const getElement = (selector) => {
-  document.querySelector(selector);
+  return document.querySelector(selector);
 };
 
 // getElements()
 function getElements(selector) {
-  document.querySelectorAll(selector);
+  return document.querySelectorAll(selector);
 }
 
 function swapFirstTwo() {
+  // Getting all the movie titles with Array
   let movieArray = getElements("#movie");
-  console.log(movieArray);
+
+  // Getting the elements inside of the parent div to get the child with the id of "movie-title"
+  const movieTitle1 = movieArray[0].querySelector("#movie-title").textContent;
+  const movieTitle2 = movieArray[1].querySelector("#movie-title").textContent;
+
+  // Switching of Movie titles for #1 and #2;
+  
+  movieArray[0].querySelector("#movie-title").textContent = movieTitle2;
+  movieArray[1].querySelector("#movie-title").textContent = movieTitle1;
+  
 }
 
-// moveDown()
+function swapLastTwo() {
+  // Getting all the movie titles with Array
+  let movieArray = getElements("#movie");
 
-// moveUp()
+  // Getting the elements inside of the parent div to get the child with the id of "movie-title"
+  const movieTitle2 = movieArray[1].querySelector("#movie-title").textContent;
+  const movieTitle3 = movieArray[2].querySelector("#movie-title").textContent;
+
+  // Switching of Movie titles for #2 and #3;
+  
+  movieArray[1].querySelector("#movie-title").textContent = movieTitle3;
+  movieArray[2].querySelector("#movie-title").textContent = movieTitle2;
+  
+}
 
 const topButton = getElement("#top_btn");
 const upButton = getElement("#up_btn");
+const downButton = getElement("#down_btn")
+const bottomButton = getElement("#bottom_btn")
 
-topButton.addEventListener("click", swapFirstTwo());
-upButton.addEventListener("click", swapFirstTwo());
+topButton.addEventListener("click", () => {swapFirstTwo()});
+upButton.addEventListener("click",() => {swapFirstTwo()});
+
+downButton.addEventListener("click",() => {swapLastTwo()});
+bottomButton.addEventListener("click",() => {swapLastTwo()});
+
 document.addEventListener("DOMContentLoaded", () => {});
+
+// Learned
+// .value is used for specifically for forms
+// textContent are used for tags like p, h1, etc.
+// using a 1 line arrow function without {} automatically returns the value
+// using multiple lines of arrow function with {} have to specify the return keyword. 
+// querySelectorAll() returns a nodeList;
+// It is better to define functions at the top; 
