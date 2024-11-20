41bfa9b99a8a1a654be16ae6f52ff742858f33f0
Carl Wico <139624156+WCARL12@users.noreply.github.com.>
2024-11-20 19:13:11 -0330

    Added

--- Assignments/Assignments-3/countdown/script.js
+++ Assignments/Assignments-3/countdown/script.js
@@ -1,8 +1,27 @@
-const eventDate = new Date("4/15/2024");
+const eventDate = new Date("11/25/2024");
 
-let dueDate = new Date();
+let todayDate = new Date(Date.now());
 
-dueDate.setDate(Date.now() - eventDate)
 
-console.log(dueDate);
+// dueDate.setDate(dueDate.getDate() - eventDate.getDate())
+// console.log(todayDate.getDate());
+// dueDate.getMilliseconds
+// eventDate.setDate(eventDate.getDate() + 30)
+// console.log(eventDate);
+// dueDate = dueDate.getTime();
+let msUntilDue = eventDate.getTime() - todayDate.getTime();
+let days = (msUntilDue / 1000) / 86400;
+
+msUntilDue = (eventDate.getTime() - todayDate.getTime());
+let hours =  (msUntilDue / 1000) / 3600;
+// let hours =  (msUntilDue / 1000) / 3600;
+
+console.log(days);
+console.log(hours);
+
+
+
+
+
+
 
